a379d3ad180a1c8274caf3c9f5436bdf125731da
Ali Al-Shaikh <mi3lix9@outlook.sa>
2024-04-07 20:22:38 +0000

    fix design

--- src/components/calculator.tsx
+++ src/components/calculator.tsx
@@ -11,7 +11,7 @@ export const Calculator: React.FC = () => {
   }, [members]);
   return (
     <form className="grid grid-rows-4 grid-cols-8">
-      <fieldset className="col-start-1 col-end-8 row-start-1 row-end-5 grid grid-rows-4 grid-cols-4 bg-sky-950 text-center text-sky-500 rounded-2xl">
+      <fieldset className="col-start-2 col-end-9 row-start-1 row-end-5 grid grid-rows-4 grid-cols-4 bg-sky-950 text-center text-sky-500 rounded-r-2xl">
         <label
           htmlFor="members"
           className="text-xl row-start-2 col-start-2 col-end-4"
@@ -26,7 +26,7 @@ export const Calculator: React.FC = () => {
           min={0}
         />
       </fieldset>
-      <fieldset className="col-start-7 col-end-9 row-start-2 row-end-4 flex flex-col justify-evenly aspect-square bg-white rounded-2xl shadow-md">
+      <fieldset className="col-start-1 col-end-3 row-start-2 row-end-4 flex flex-col justify-evenly aspect-square bg-white rounded-2xl shadow-md">
         <span className="text-sky-500 text-center">الزكاة</span>
         <span className="text-center">{result}</span>
       </fieldset>
